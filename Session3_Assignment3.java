8f8c08e7abaadaa2f537e8b6fea517f7085d5114
arpchauh <arpitchauhan10@gmail.com>
2016-09-29 11:17:49 +0530

    Add files via upload

---
+++ Session3_Assignment3.java
@@ -0,0 +1,14 @@
+package tanaji;
+
+public class Session3_Assignment3 {
+	
+			 int x; int y;
+			 String name;
+			 public static void main(String args[]) {
+				 Session3_Assignment3 pnt = new Session3_Assignment3();
+			 System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+			} 
+
+	}
+
+//d. The program prints pnt is null 0 0
